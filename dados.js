522634f27433e1683ca083ce0e97cc2f147e5e60
Leandro <90810735+Lelibi@users.noreply.github.com>
2024-09-07 22:23:25 -0300

    Update dados.js

---
+++ dados.js
@@ -0,0 +1,33 @@
+let dados = [
+    {
+        titulo: "Alan Fonteles",
+        descricao: "Atleta de atletismo, conhecido por sua velocidade e determinação. Representou o Brasil em diversas competições.",
+        link: "https://pt.wikipedia.org/wiki/Alan_Fonteles",
+        tags: "atletismo velocista paralímpico"
+    },
+    {
+        titulo: "Evelyn de Oliveira",
+        descricao: "Atleta de bocha, medalhista de ouro nas Paralimpíadas do Rio 2016. Sua história de superação inspira muitos.",
+        link: "https://pt.wikipedia.org/wiki/Evelyn_de_Oliveira",
+        tags: "bocha paralímpica medalhista"
+    },
+    {
+        titulo: "Daniel Dias",
+        descricao: "Nadador paralímpico, considerado um dos maiores atletas brasileiros de todos os tempos.",
+        link: "https://pt.wikipedia.org/wiki/Daniel_Dias",
+        tags: "natação paralímpico medalhista"
+    },
+    {
+        titulo: "Terezinha Guilhermina",
+        descricao: "Atleta de atletismo, conhecida como a Furacão Brasileiro. Uma das maiores corredoras paralímpicas do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Terezinha_Guilhermina",
+        tags: "atletismo velocista paralímpica"
+    },
+    {
+        titulo: "Claudiney Batista",
+        descricao: "Atleta de atletismo, especialista em provas de longa distância. Representou o Brasil em diversas Paralimpíadas.",
+        link: "https://pt.wikipedia.org/wiki/Claudiney_Batista",
+        tags: "atletismo maratonista paralímpico"
+    }
+];
+
